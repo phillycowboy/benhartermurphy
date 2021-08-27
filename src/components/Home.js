b9c30ca4b5c0895777e7002dd868d85341dff274
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-08-26 20:24:37 -0400

    landing page is complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,26 @@ class Home extends Component{
                 <div id="name-div">
                     <h1 id="name" class="slide-in-right">Ben Harter-Murphy</h1>
                     <h1 id="title" class="slide-in-right">Full Stack Web Developer</h1>
+                    <div id="languages" class="slide-in-bottom">
+                        <ul>
+                            <li><i class="fab fa-html5"></i></li>
+                            <li><i class="fab fa-css3-alt"></i></li>
+                            <li><i class="fab fa-js-square"></i></li>
+                            <li><i class="fab fa-react"></i></li>
+                            <li><i class="icon-ruby-on-rails"></i></li>
+                        </ul>
+
+                    </div>
                 </div>
+                {/* <div id="languages">
+                    <ul>
+                        <li><i class="fab fa-html5"></i></li>
+                        <li><i class="fab fa-css3-alt"></i></li>
+                        <li><i class="fab fa-js-square"></i></li>
+                        <li><i class="fab fa-react"></i></li>
+                        <li><i class="icon-ruby-on-rails"></i></li>
+                    </ul>
+                </div> */}
             </div>
         )
     }
