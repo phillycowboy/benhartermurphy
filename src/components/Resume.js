f991d54c7a6d7fff9da2a607fdadc10545107b4c
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-02 13:42:59 -0400

    what?

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-// import {Document} from 'react-pdf'
 
 class Resume extends Component{
     render(){
         return(
             <div id="resume">
                 <h1>RESUME</h1>
-                <p>(It's A Link Too!)</p>
+                <p>It's A Link Too!</p>
                 <a href="https://resume.creddle.io/resume/ikk1mkeet2m"><img src="resume screen shot.png" /></a>
             </div>
         )
