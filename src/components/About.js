185f2c2adb908b6559b6333e5fc44c3eee9d9647
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-02 15:18:16 -0400

    edits

--- src/components/About.js
+++ src/components/About.js
@@ -5,18 +5,18 @@ class About extends Component{
         return(
             <div id="about">
                 <h1>ABOUT ME</h1>
-                <h5>Hello! My name is Ben Harter-Murphy and I am Full Stack Web Developer with a focus on Front End Development and Design.</h5>
-                <h5>I was first introduced to programming and design while I was employed as a recruiter in Philadelphia. I was fascinated by their work and how they got their ideas from concept to a literal webpage.
-                    Unfortunately due to the pandemic alot of us were let go. But then my curiousity kicked in. I started taking minimal courses on HTML, CSS, and Vanilla JavaScript, after that I was hooked. That's when I made the 
-                    decision to attend the Online Software Engineering Programming at Flatiron School and delve deeper into Programming.
+                <h5>Hello! My name is Ben Harter-Murphy and I am Full Stack Web Developer with a focus on Front End Development and Front End Design.</h5>
+                <h5>I was first introduced to Programming and Design while I was employed as an IT Recruiter in Philadelphia. I was fascinated by their work, and how they got their ideas to transform into literal webpages.
+                    Unfortunately, because of the pandemic alot of us Recruiters were let go. But then my curiousity kicked in. I started taking beginner courses on HTML, CSS, and JavaScript, after that I was hooked. That's when I made the 
+                    decision to attend the Software Engineering Program at Flatiron School, and dive deeper into Programming.
                 </h5>
                 <h5>
-                    While attending Flatiron we progressed through five modules, starting in the Back End with Ruby and Ruby on Rails. Then by the fifth module we were building Full Stack Web Applications
-                    with a JavaScript(ES6) Front End with a React library.
+                    While attending Flatiron we progressed through five modules. Starting in the Back End with Ruby, and Ruby on Rails. Then by the fifth module we were building Full Stack Web Applications
+                    utilizing JavaScript(ES6) and React for the Front End.
                 </h5>
                 <h5>
-                    If you are intrested in the projects that I built in my time at Flation please navigate to the Projects page and take a look around. 
-                    If you need to get in touch with me please navigate to the Contact page where you can find my information.
+                    If you are intrested in the projects that I built during my time at Flatiron, please navigate to the Projects page and take a look around. 
+                    If you need to get in touch with me, please navigate to the Contact page where you can find my information.
                 </h5>
                 <h5>Thanks for stopping by!</h5>
 
