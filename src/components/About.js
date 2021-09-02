f9c55846976b0082868fa4064a304c063350d8a6
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-02 12:57:10 -0400

    about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,20 @@ class About extends Component{
         return(
             <div id="about">
                 <h1>ABOUT ME</h1>
-                <h5> This is my about change</h5>
+                <h5>Hello! My name is Ben Harter-Murphy and I am Full Stack Web Developer with a focus on Front End Development and Design.</h5>
+                <h5>I was first introduced to programming and design while I was employed as a recruiter in Philadelphia. I was fascinated by their work and how they got their ideas from concept to a literal webpage.
+                    Unfortunately due to the pandemic alot of us were let go. But then my curiousity kicked in. I started taking minimal courses on HTML, CSS, and Vanilla JavaScript, after that I was hooked. That's when I made the 
+                    decision to attend the Online Software Engineering Programming at Flatiron School and delve deeper into Programming.
+                </h5>
+                <h5>
+                    While attending Flatiron we progressed through five modules, starting in the Back End with Ruby and Ruby on Rails. Then by the fifth module we were building Full Stack Web Applications
+                    with a JavaScript(ES6) Front End with a React library.
+                </h5>
+                <h5>
+                    If you are intrested in the projects that I built in my time at Flation please navigate to the Projects page and take a look around. 
+                    If you need to get in touch with me please navigate to the Contact page where you can find my information.
+                </h5>
+                <h5>Thanks for stopping by!</h5>
 
             </div>
         )
