dd0812eb44fe2e25433aa8f5d4b04768c7293417
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-02 13:40:50 -0400

    resume img

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+// import {Document} from 'react-pdf'
 
 class Resume extends Component{
     render(){
         return(
             <div id="resume">
                 <h1>RESUME</h1>
-                <h5>maybe put your pdf resume here as a photo and then wrap it in the nav color and then box shadow the blue colors</h5>
+                <p>(It's A Link Too!)</p>
+                <a href="https://resume.creddle.io/resume/ikk1mkeet2m"><img src="resume screen shot.png" /></a>
             </div>
         )
     }
