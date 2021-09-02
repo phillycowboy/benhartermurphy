f5215733673776441eb4ffb6defcf2ed0d1fb09d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-02 16:48:39 -0400

    edits

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ class About extends Component{
         return(
             <div id="about">
                 <h1>ABOUT ME</h1>
-                <h5>Hello! My name is Ben Harter-Murphy and I am Full Stack Web Developer with a focus on Front End Development and Front End Design.</h5>
-                <h5>I was first introduced to Programming and Design while I was employed as an IT Recruiter in Philadelphia. I was fascinated by their work, and how they got their ideas to transform into literal webpages.
+                <h5>Hello! My name is Ben Harter-Murphy and I am a Full Stack Web Developer with a focus on Front End Development and Front End Design.</h5>
+                <h5>I was first introduced to Programming and Design while I was employed as an IT Recruiter in Philadelphia. I was fascinated by their work and how they got their ideas to transform into literal webpages.
                     Unfortunately, because of the pandemic alot of us Recruiters were let go. But then my curiousity kicked in. I started taking beginner courses on HTML, CSS, and JavaScript, after that I was hooked. That's when I made the 
-                    decision to attend the Software Engineering Program at Flatiron School, and dive deeper into Programming.
+                    decision to attend the Software Engineering Program at Flatiron School and dive deeper into Programming.
                 </h5>
                 <h5>
-                    While attending Flatiron we progressed through five modules. Starting in the Back End with Ruby, and Ruby on Rails. Then by the fifth module we were building Full Stack Web Applications
+                    While attending Flatiron we progressed through five modules. Starting in the Back End with Ruby and Ruby on Rails. Then by the fifth module we were building Full Stack Web Applications
                     utilizing JavaScript(ES6) and React for the Front End.
                 </h5>
                 <h5>
