1f50bfc018f26ca3e83ca2c63ab23c559b89be2a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-09-02 10:30:05 -0400

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,7 @@ class About extends Component{
         return(
             <div id="about">
                 <h1>ABOUT ME</h1>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in lectus quis leo venenatis iaculis sit amet ac nisi. Duis sem magna, gravida ac est vel, efficitur sodales enim. Vivamus sed facilisis urna. Integer vitae ex nisl. Donec egestas et libero ut vestibulum. Proin purus ipsum, condimentum eu ante eget, accumsan fermentum nibh. Donec sodales tellus quis nisi mollis rhoncus. Donec a egestas lorem. Phasellus pellentesque mauris suscipit, tristique ante vel, eleifend ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed pretium velit non arcu pulvinar, in bibendum tortor aliquet.
-
-                    Nunc posuere urna ut tincidunt malesuada. Nullam a mi varius, accumsan erat id, luctus enim. Sed vel risus purus. Praesent sit amet rhoncus massa. Proin feugiat et arcu sed tempus. Sed luctus augue at dictum pulvinar. Pellentesque commodo mi quam, a sagittis quam blandit sollicitudin. Etiam condimentum elit eros, vehicula pellentesque arcu tempor eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac mi ut diam bibendum auctor at vitae dolor. Vestibulum porttitor rutrum sollicitudin. Integer ultricies velit eget viverra porttitor. Aenean dui ex, volutpat et commodo in, rhoncus et felis.</h5>
-                {/* <div id="about-style-div"></div> */}
+                <h5> This is my about change</h5>
 
             </div>
         )
